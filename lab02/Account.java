3b6020a843beb6c5126eda7858055b6adadeb4ee
Evan Day <yichaoday_evan@berkeley.edu>
2022-06-23 02:26:20 -0700

    edit and add two constructors

--- lab02/Account.java
+++ lab02/Account.java
@@ -10,6 +10,12 @@ public class Account {
     /** Initialize an account with the given balance. */
     public Account(int balance, Account parentAccount) {
         this.balance = balance;
+        this.parentAccount = parentAccount;
+    }
+
+    /** Initialize an account with the given balance. 2nd */
+    public Account(int balance) {
+        this.balance = balance;
         this.parentAccount = null;
     }
 
@@ -34,17 +40,40 @@ public class Account {
      */
     public boolean withdraw(int amount) {
         // TODO
+        if (parentAccount == null){
+             if (amount < 0) {
+            System.out.println("Cannot withdraw negative amount.");
+            return false;
+        } else if (balance >= amount) {
+            balance -= amount;
+            return true;
+        } else {
+             System.out.println("Insufficient funds");
+             return false;
+         }
+     } else {
+
         if (amount < 0) {
             System.out.println("Cannot withdraw negative amount.");
             return false;
-        } else if (balance < amount) {
-            System.out.println("Insufficient funds");
-            return false;
-        } else {
+        } else if (balance >= amount) {
             balance -= amount;
             return true;
+        } else {
+             if (parentAccount == null) {
+                System.out.println("Insufficient funds");
+                return false;
+            } else if (parentAccount != null && (balance + parentAccount.balance) >= amount) {
+                parentAccount = parentAccount.balance + balance - amount;
+                balance = 0;
+                return true;
+            } else {
+                System.out.println("Insufficient funds");
+                return false;
+            }
         }
-    }
+     }
+
 
     /**
      * Merge account other into this account by removing all money from other
