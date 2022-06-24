eb7d0040a1eee5347990f4e7fbe1b19c6839ab6e
Evan Day <yichaoday_evan@berkeley.edu>
2022-06-24 10:27:21 -0700

    finish lab3

--- lab03/ArrayOperations.java
+++ lab03/ArrayOperations.java
@@ -9,6 +9,14 @@ public class ArrayOperations {
             return;
         }
         // TODO: YOUR CODE HERE
+        for(int i = pos; i <= values.length-1; i++){
+            if (i == values.length-1) {
+                values[i] = 0;
+            } else {
+                    values[i] = values[i+1];
+            }
+
+        }
     }
 
     /**
@@ -21,15 +29,29 @@ public class ArrayOperations {
             return;
         }
         // TODO: YOUR CODE HERE
+        for(int i = values.length-1; i > pos; i--) {
+            values[i] = values[i-1];
+        }
+        values[pos] = newInt;
     }
 
-    /** 
+
+    /**
      * Returns a new array consisting of the elements of A followed by the
-     *  the elements of B. 
+     *  the elements of B.
      */
     public static int[] catenate(int[] A, int[] B) {
         // TODO: YOUR CODE HERE
-        return null;
+        int totalLength = A.length + B.length;
+        int[] newArray = new int[totalLength];
+        for (int i = 0; i < totalLength; i++){
+            if (i < A.length) {
+                newArray[i] = A[i];
+            } else {
+                newArray[i] = B[i - A.length];
+            }
+        }
+        return newArray;
     }
 
 }
