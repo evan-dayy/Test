3a3be4776a34823419fb4c44d6a12455019fe33c
Evan Day <yichaoday_evan@berkeley.edu>
2022-06-24 15:32:06 -0700

    First Constructor V1.0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -29,6 +29,7 @@ public class Model extends Observable {
      *  and score 0. */
     public Model(int size) {
         // TODO: Fill in this constructor.
+         this._board = new Board(size);
 
     }
 
